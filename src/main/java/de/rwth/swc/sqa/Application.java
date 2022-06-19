3ed1f7e286a06136079693f583891f8205e3e01a
Tran Vu Tuan <Vu-Tuan.Tran@aspera.com>
2022-06-19 20:46:54 +0200

    Fix 1.

--- src/main/java/de/rwth/swc/sqa/Application.java
+++ src/main/java/de/rwth/swc/sqa/Application.java
@@ -1,11 +1,23 @@
 package de.rwth.swc.sqa;
 
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+
+    @Bean
+    public MappingJackson2HttpMessageConverter mappingJackson2HttpMessageConverter() {
+        ObjectMapper mapper = new ObjectMapper();
+
+        mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, true);
+        return new MappingJackson2HttpMessageConverter(mapper);
     }
 }
